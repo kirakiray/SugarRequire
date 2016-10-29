aaf96501e76076ac0073b8f247b1731f54dcf1e8
pikay <pikay@foxmail.com>
2016-10-30 00:14:28 +0800

    Improve the "srdebug" plugin
    Improve the "srdebug" plugin
    完善“srdebug”插件
    添加srdebug测试页面

--- plugin/sr-debug.js
+++ plugin/sr-debug.js
@@ -2,4 +2,26 @@ sr.extend(function(baseResources, R, SugarRequire) {
     window.baseResources = baseResources;
     window.R = R;
     window.SugarRequire = SugarRequire;
+
+    var a = null;
+    //旧的函数
+    var beforeSugarRequireInit = SugarRequire.fn.init;
+    //继承旧方法
+    SugarRequire.fn.init = function() {
+        //优先继承初始化
+        beforeSugarRequireInit.apply(this, arguments);
+
+        //主动触发报错机制
+        try {
+            a();
+        } catch (e) {
+            //分组
+            var errArr = e.stack.match(/[a-zA-Z]+?:\/\/.+\d/g);
+            //错误后输出结果
+            this.fail(function() {
+                //辅助打印错误引入的定位
+                console.error("fail positioning => ", errArr[3]);
+            });
+        }
+    };
 });
