9f2ffbb8acd0b7e829475896971e1f31cd370945
pikay <pikay@foxmail.com>
2016-11-09 22:09:48 +0800

    Upgrade the 'sr-nocache'plugin
    Added more functionality to sr-nocache
    增强了sr-nocache插件

--- plugin/sr-nocache.js
+++ plugin/sr-nocache.js
@@ -1,8 +1,23 @@
 //缓存版本控制器
+// 有srentry入口标记存在会自动设置版本号
 sr.extend(function(baseResources, R, SugarRequire) {
     var beforeGetPath = R.getPath;
+    //获取页面元素版本
+    var v = new Date().getTime();
+    if (document.querySelector) {
+        var entryScript = document.querySelector('[srentry]');
+        if (!entryScript) {
+            return;
+        }
+        //获取版本号key
+        var nocacheKey = entryScript.getAttribute('srentry') || "v";
+        var verreg = new RegExp(nocacheKey + "=(\\d+)");
+        //根据key设置版本号
+        var verarr = entryScript.src.match(verreg);
+        v = verarr[1];
+    }
     sr.Nocache = {
-        v: new Date().getTime()
+        v: v
     };
     R.getPath = function() {
         var pathStr = beforeGetPath.apply(R, arguments);
